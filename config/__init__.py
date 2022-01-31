10ef3d84ed4733f5c9a4c444b301384a174955cf
sahildatax <84317311+sahildatax@users.noreply.github.com>
2022-01-31 18:05:37 +0530

    Add files via upload

---
+++ config/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    API_ID = 1234
+    API_HASH = "21ab7cb0a453b5e60016dc7bbeb701cb"
+    CHANNEL = "-1001249461809"
+    CHANNEL_USERNAME = "sahil_nolia"
+    TOKEN = "token"
+    DOMAIN  = os.getenv("domain","https://streamboty.nolia.repl.co")
